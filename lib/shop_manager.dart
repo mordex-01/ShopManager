eda13c8bebecac7a3a801bae9294e8220b48322a
mordex-01 <jalal.zarei22@gmail.com>
2024-01-18 05:48:05 -0800

    its almost done

--- lib/shop_manager.dart
+++ lib/shop_manager.dart
@@ -22,7 +22,8 @@ class ShopActions {
         userInput != 6 &&
         userInput != 2 &&
         userInput != 3 &&
-        userInput != 4) {
+        userInput != 4 &&
+        userInput != 5) {
       print("Try to input Valid number");
       printMenu();
       userInput = inputUser();
@@ -40,6 +41,10 @@ class ShopActions {
     }
     if (userInput == 4) {
       ProductActions.editProducts();
+      ShopActions.backToShopMenu();
+    }
+    if (userInput == 5) {
+      ProductActions.finishidProducts();
       ShopActions.backToShopMenu();
     }
     if (userInput == 6) {
@@ -123,7 +128,12 @@ class ProductActions {
     }
     print("Enter ${Shop.productsList.length} To Cancel");
     print("Enter product ID to delete :");
-    int id = int.parse(stdin.readLineSync()!);
+    String? userInput = stdin.readLineSync();
+    if (int.tryParse(userInput!) == null) {
+      print("Cancelled");
+      ShopActions.backToShopMenu();
+    }
+    int id = int.parse(userInput);
     //check if user wants to back to menu
     if (id == Shop.productsList.length) {
       print("Cancelled");
@@ -141,13 +151,20 @@ class ProductActions {
 
   static editProducts() {
     print("Edit Products :");
+
     for (var Shop in Shop.productsList) {
       print(
           "ID:${Shop.id} Name : ${Shop.productName}, Price : ${Shop.price}T, Count : ${Shop.numberOfProducts}\n ");
     }
     print("Enter ${Shop.productsList.length} To Cancel");
     print("Enter product ID to Edit :");
-    int id = int.parse(stdin.readLineSync()!);
+    String? userInput = stdin.readLineSync();
+    if (int.tryParse(userInput!) == null) {
+      print("Cancelled");
+      ShopActions.backToShopMenu();
+    }
+    int id = int.parse(userInput);
+
     //check if user wants to back to menu
     while (id == Shop.productsList.length) {
       print("Cancelled");
@@ -164,11 +181,31 @@ class ProductActions {
       print("Cancelled");
       ShopActions.backToShopMenu();
     }
+    //check if name == null or empty
+    if (name == null || name.isEmpty) {
+      print("(Error)Enter Valid Name :");
+      return ProductActions.productRegistration();
+    }
+    //check if name is a number
+    if (int.tryParse(name) != null) {
+      print("(Error)Enter Valid Name :");
+      return ProductActions.productRegistration();
+    }
     print("(Enter the number of products)(0-back to menu)");
     String? number = stdin.readLineSync();
     if (number == "0") {
       print("Cancelled");
       ShopActions.backToShopMenu();
+    }
+    //check if number == null or empty
+    if (number == null || number.isEmpty) {
+      print("(Error)Enter Valid Number :");
+      return ProductActions.productRegistration();
+    }
+    //check if number contains a letter
+    if (int.tryParse(number) == null) {
+      print("(Error)Enter Valid Number :");
+      return ProductActions.productRegistration();
     }
     print("(Enter the Price)(0-back to menu)");
     String? price = stdin.readLineSync();
@@ -176,8 +213,27 @@ class ProductActions {
       print("Cancelled");
       ShopActions.backToShopMenu();
     }
-    Shop.productsList[id] =
-        Shop(name!, int.parse(number!), int.parse(price!), id);
+    //check if price == null or empty
+    if (price == null || price.isEmpty) {
+      print("(Error)Enter Valid Price :");
+      return ProductActions.productRegistration();
+    }
+    //check if number contains a letter
+    if (int.tryParse(price) == null) {
+      print("(Error)Enter Valid Price :");
+      return ProductActions.productRegistration();
+    }
+    Shop.productsList[id] = Shop(name, int.parse(number), int.parse(price), id);
+  }
+
+  static finishidProducts() {
+    print("Products Less Than 5 count :");
+    for (var Shop in Shop.productsList) {
+      if (Shop.numberOfProducts < 5) {
+        print(
+            "ID:${Shop.id} Name : ${Shop.productName}, Price : ${Shop.price}T, Count : ${Shop.numberOfProducts}\n ");
+      }
+    }
   }
 }
 
